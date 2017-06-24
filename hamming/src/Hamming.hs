6d31757443f2fe2d5507e382aa6f10cece15bec9
Tim Medina <iamteem@gmail.com>
2017-06-24 19:06:19 +0800

    Hamming v2: don't use length.

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -1,12 +1,9 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys
-  | length xs == length ys = distance' xs ys
-  | otherwise = Nothing
-
-distance' :: String -> String -> Maybe Int
-distance' [] [] = Just 0
-distance' (x:xs) (y:ys)
-  | x == y = distance' xs ys
-  | otherwise = (+1) <$> distance' xs ys
+distance [] (_:_) = Nothing
+distance (_:_) [] = Nothing
+distance [] [] = Just 0
+distance (x:xs) (y:ys)
+  | x == y = distance xs ys
+  | otherwise = (+1) <$> distance xs ys
